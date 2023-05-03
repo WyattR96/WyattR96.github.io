4d3bb88730383f507e6c96a69d0da913b4d7973b
cierraarmijo <123608375+cierraarmijo@users.noreply.github.com>
2023-05-02 20:21:09 -0600

    Update Products.py

--- Part4/Products.py
+++ Part4/Products.py
@@ -1,13 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def my_items():
-    items = {}
-    items["flavors"] = ['Blackberry', 'Chocolate', 'Cookies and Cream', 'Cotton Candy', 'Mango', 'Pistachio', 'Toffee', 'Strawberry', 'Vanilla']
-    items["prices"] = [1.99, 1.99, 1.99, 1.99, 1.99, 1.99, 1.99, 1.99, 1.99]
-    return jsonify(items)
+def home():
+  return render_template("Main-Page.html")
+
+#API for flavors/prices?
+#@app.route("/")
+#def my_items():
+#    items = {}
+#    items["flavors"] = ['Blackberry', 'Chocolate', 'Cookies and Cream', 'Cotton Candy', 'Mango', 'Pistachio', 'Toffee', 'Strawberry', 'Vanilla']
+#    items["prices"] = [1.99, 1.99, 1.99, 1.99, 1.99, 1.99, 1.99, 1.99, 1.99]
+#   return jsonify(items)
 
 
 
