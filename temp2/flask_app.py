a23bb47c7783cc9b94d526942d8c7fc000f8bdc7
cierraarmijo <123608375+cierraarmijo@users.noreply.github.com>
2023-05-08 12:26:18 -0600

    Update flask_app.py

--- temp2/flask_app.py
+++ temp2/flask_app.py
@@ -4,13 +4,14 @@ from flask_mail import Mail, Message
 import flask_sqlalchemy
 
 app = Flask(__name__)
+mail = Mail(app)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_PASSWORD'] = 'qzuhswckzguinwiq'
-app.config['MAIL_USERNAME'] = 'cs334teamproject@gmail.com'
+app.config['MAIL_PASSWORD'] = 'cgbpzdflvhpukjbe' #qzuhswckzguinwiq'
+app.config['MAIL_USERNAME'] = 'yourfavoriteicecreamshop@gmail.com'
+#app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = True
-
 
 mail = Mail(app)
 
@@ -44,7 +45,7 @@ def itemManagement():
 def result():
     if request.method == 'POST':
         name = request.form.get("Name")
-        msg = Message('Your Favorite Ice Cream Shop Order Confirmation', sender='cs334teamproject@gmail.com', recipients=[request.form.get("Email")])
+        msg = Message('Your Favorite Ice Cream Shop Order Confirmation', sender='yourfavoriteicecreamshop@gmail.com', recipients=[request.form.get("Email")])
         msg.body = 'Thank you for your order ' + name + '!'
         mail.send(msg)
         return render_template('SubmitForm.html', result='Success!')
@@ -72,3 +73,119 @@ def serviceWorker():
 if __name__ == '__main__':
     app.run(debug=True)
     
+    
+    '''
+    
+    from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+from sqlalchemy import ForeignKey
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+db = SQLAlchemy(app)
+app.secret_key="mySecret"
+
+class Flavors(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(50))
+    name = db.Column(db.String(50))
+
+
+class Customers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+
+
+class Employees(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+
+class Orders(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    items = db.Column(db.PickleType())
+
+
+@app.route("/")
+def main():
+    fla = Flavors.query.all()
+    return render_template('Main-Page.html', flavors=fla)
+
+
+@app.route("/Main-Page.html")
+def mainPage():
+    fla = Flavors.query.all()
+    return render_template('Main-Page.html', flavors=fla)
+
+
+@app.route("/Login.html")
+def login():
+    return render_template('Login.html')
+
+
+@app.route("/Checkout.html")
+def checkout():
+    return render_template('Checkout.html')
+
+
+@app.route("/Item-Management.html", methods=['GET', 'POST'])
+def itemManagement():
+    fla = Flavors.query.all()
+
+    if request.method == 'POST':
+        if 'title' in request.form and 'URL' in request.form:
+            Name = request.form['title']
+            Url = request.form['URL']
+            newFlavor = Flavors(name=Name,url=Url)
+            db.session.add(newFlavor)
+            db.session.commit()
+
+        if 'deleteID' in request.form:
+            Id = request.form['deleteID']
+            delete = Flavors.query.filter_by(id=Id).first()
+            db.session.delete(delete)
+            db.session.commit()
+
+        if 'updateId' in request.form:
+            print("asdf")
+            Id = request.form['updateId']
+            newName = request.form['updateName']
+            newUrl = request.form['updateUrl']
+            flavor = Flavors.query.filter_by(id=Id).first()
+            flavor.name = newName
+            flavor.url = newUrl
+            db.session.commit()
+
+    return render_template('Item-Management.html', flavors=fla)
+
+
+
+@app.route("/SubmitForm.html")
+def submit():
+    return render_template('SubmitForm.html')
+
+
+@app.route("/manifest.json")
+def manifest():
+    return send_from_directory('static', 'manifest.json')
+
+
+@app.route("/js/app.js")
+def app_js():
+    return send_from_directory('static', 'js/app.js')
+
+
+@app.route("/serviceWorker.js")
+def serviceWorker():
+    return send_from_directory('static', 'js/serviceWorker.js')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)'''
