2ebcda47295424a9988dedee5a5a1354fcd7ccc9
Wyatt Robinson <98620247+WyattR96@users.noreply.github.com>
2023-05-08 17:28:02 -0500

    Add files via upload

--- temp3/flask_app.py
+++ temp3/flask_app.py
@@ -34,11 +34,13 @@ class Customers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     email = db.Column(db.String(50))
+    password = db.Column(db.String(64))
 
 
 class Employees(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
+    password = db.Column(db.String(64))
 
 
 class Orders(db.Model):
