00385c8fe0bea587feb5a0e900ab30bbc922defe
Wyatt Robinson <98620247+WyattR96@users.noreply.github.com>
2023-05-08 20:50:49 -0500

    Add files via upload

--- temp3/flask_app.py
+++ temp3/flask_app.py
@@ -1,28 +1,29 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory, session
 from flask_mail import Mail, Message
 from flask_sqlalchemy import SQLAlchemy
 import os
-
+from datetime import timedelta
 
 app = Flask(__name__)
 mail = Mail(app)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_PASSWORD'] = 'cgbpzdflvhpukjbe' #qzuhswckzguinwiq'
+app.config['MAIL_PASSWORD'] = 'cgbpzdflvhpukjbe'  # qzuhswckzguinwiq'
 app.config['MAIL_USERNAME'] = 'yourfavoriteicecreamshop@gmail.com'
-#app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = True
 
 mail = Mail(app)
-
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
+
 db = SQLAlchemy(app)
-app.secret_key="mySecret"
+app.secret_key = "mySecret"
+
 
 class Flavors(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -48,25 +49,41 @@ class Orders(db.Model):
     items = db.Column(db.PickleType())
 
 
-
-
-
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def main():
     fla = Flavors.query.all()
     return render_template('Main-Page.html', flavors=fla)
 
-@app.route("/Main-Page.html")
+
+@app.route("/Main-Page.html", methods=['GET','POST'])
 def mainPage():
     fla = Flavors.query.all()
+
+    if request.method == 'POST':
+        item = request.form['select']
+        flavor = Flavors.query.filter(Flavors.name == item).first().name
+        return render_template('form.html',items=flavor)
+
     return render_template('Main-Page.html', flavors=fla)
 
-@app.route("/Login.html")
+
+@app.route("/Login.html", methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        if 'User' in request.form and 'Pass' in request.form:
+            Name = request.form['User']
+            password = request.form['Pass']
+            customer = db.session.query(Customers.id).filter_by(name=Name).first()
+            if customer is not None:
+                print(customer.password)
+                fla = Flavors.query.all()
+                return render_template('Main-Page.html', flavors=fla)
+
     return render_template('Login.html')
 
-@app.route("/Checkout.html", methods=['GET', 'POST'])
-def checkout():
+
+@app.route("/form.html", methods=['GET', 'POST'])
+def checkout(items):
     return render_template('form.html')
 
 
@@ -78,7 +95,7 @@ def itemManagement():
         if 'title' in request.form and 'URL' in request.form:
             Name = request.form['title']
             Url = request.form['URL']
-            newFlavor = Flavors(name=Name,url=Url)
+            newFlavor = Flavors(name=Name, url=Url)
             db.session.add(newFlavor)
             db.session.commit()
 
@@ -101,14 +118,12 @@ def itemManagement():
     return render_template('Item-Management.html', flavors=fla)
 
 
-
-
-
-@app.route("/SubmitForm", methods = ['POST', 'GET'])
+@app.route("/SubmitForm", methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
         name = request.form.get("Name")
-        msg = Message('Your Favorite Ice Cream Shop Order Confirmation', sender='yourfavoriteicecreamshop@gmail.com', recipients=[request.form.get("Email")])
+        msg = Message('Your Favorite Ice Cream Shop Order Confirmation', sender='yourfavoriteicecreamshop@gmail.com',
+                      recipients=[request.form.get("Email")])
         msg.body = 'Thank you for your order ' + name + '!'
         mail.send(msg)
         return render_template('SubmitForm.html', result='Success!')
@@ -116,27 +131,24 @@ def result():
         return render_template('SubmitForm.html', result='Fail')
 
 
-
 @app.route("/manifest.json")
 def manifest():
-    return send_from_directory('static','manifest.json')
+    return send_from_directory('static', 'manifest.json')
+
 
 @app.route("/js/app.js")
 def app_js():
-    return send_from_directory('static','js/app.js')
+    return send_from_directory('static', 'js/app.js')
+
 
 @app.route("/serviceWorker.js")
 def serviceWorker():
-    return send_from_directory('static','js/serviceWorker.js')
+    return send_from_directory('static', 'js/serviceWorker.js')
 
 
-#added to prevent server error
+# added to prevent server error
 with app.app_context():
     db.create_all()
 
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-
